463dad390776d71fa5bca37a46bf5390e3f3c138
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-04-25 11:35:54 -0300

    Added basic enemy

---
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        float colDif = collision.transform.position.y - 0.05f;
+        float myDif = transform.position.y + 0.05f;
+
+        if (colDif < myDif)
+            Destroy(collision.gameObject);
+        else
+            Destroy(gameObject);
+    }
+}
